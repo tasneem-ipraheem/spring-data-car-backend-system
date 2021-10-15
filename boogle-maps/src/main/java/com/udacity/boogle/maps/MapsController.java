8a7a4500fd6aa5db25ccce19998b047bfd583e4b
tasneem <tasneemipraheem@gmail.com@users.noreply.github.com>
2021-10-16 01:44:20 +0200

    boogel maps : request & responce example

--- boogle-maps/src/main/java/com/udacity/boogle/maps/MapsController.java
+++ boogle-maps/src/main/java/com/udacity/boogle/maps/MapsController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/maps")
 public class MapsController {
 
+	/*
+	 * ex 
+	 * req : http://localhost:9191/maps?lat=1.999&lon=9.555
+	 * res : {"address":"1415 7Th Street South","city":"Clanton","state":"AL","zip":"35045"}
+	 */
     @GetMapping
     public Address get(@RequestParam Double lat, @RequestParam Double lon) {
         return MockAddressRepository.getRandom();
