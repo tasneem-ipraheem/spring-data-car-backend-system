e9c368b016c6692d30f8184aa520d9aacbe30c36
tasneem <tasneemipraheem@gmail.com@users.noreply.github.com>
2021-10-15 23:30:27 +0200

    exeption add annotation - not used yet

--- pricing-service/src/main/java/com/udacity/pricing/service/PriceException.java
+++ pricing-service/src/main/java/com/udacity/pricing/service/PriceException.java
@@ -1,8 +1,14 @@
 package com.udacity.pricing.service;
 
-public class PriceException extends Exception {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-    public PriceException(String message) {
-        super(message);
-    }
+//https://classroom.udacity.com/nanodegrees/nd035/parts/49128b96-8489-4719-a3ea-c0beaaa46ff9/modules/585c4b4f-654f-4a9a-acbd-0fe5576a42ae/lessons/f9a4fae6-81e6-4871-95df-dcd5ba7536a2/concepts/ff7698b0-5bca-4c12-bced-4aaa6f5621cd
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Id not found")
+public class PriceException extends RuntimeException  {
+
+	public PriceException(String message) {
+		super(message);
+	}
 }
